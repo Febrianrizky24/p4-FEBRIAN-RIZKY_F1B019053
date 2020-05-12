68737e4607cd7c1a09c57d1468ad2b8ece5ad439
FEBRIAN RIZKY_F1B019053 <64627935+Febrianrizky24@users.noreply.github.com>
2020-05-12 18:57:14 +0800

    Create jobsheet10.cpp
    Buatlah program matriks tiga dimensi dengan menggunakan nim tiga orang mahasiswa

---
+++ jobsheet10.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"nama:febrian rizky"<<endl;
+	cout<<"nim:F1B019053"<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+char nama [2][2][10];
+int i, j;
+for (i = 0; i< 2; i++){
+for (j = 1; j < 10; j++){
+cout<< "nomor punggung player nba  [" <<i<<"][" << j << "] = ";
+cin>>nama[i][j];
+}
+cout<<endl;
+}
+}
