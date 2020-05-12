0b4cdfdc6856f90e0c54108a593de57ee4ee668c
FEBRIAN RIZKY_F1B019053 <64627935+Febrianrizky24@users.noreply.github.com>
2020-05-12 18:55:59 +0800

    Create jobsheet9.cpp
    Buatlah program array sebagai karakter dengan menampilkan nama setiap Mahasiswa

---
+++ jobsheet9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout<<"nama:febrian rizky"<<endl;
+	cout<<"nim:F1B019053"<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+
+int i;
+char nama[5]={'N','B','A',};
+char nama2[6]="NBA";
+cout<<"Array per Karakter= ";
+for(i=0;i<=4;i++)
+{
+cout<<nama[i];
+}
+cout<<endl; cout<<"Array string ="<<nama2<<endl<<endl;
+return 0;
+}
