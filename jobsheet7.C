9cd672ab34c2aeb66b46b86fee5d5949cb3ddde6
FEBRIAN RIZKY_F1B019053 <64627935+Febrianrizky24@users.noreply.github.com>
2020-05-12 18:48:46 +0800

    Create jobsheet7.C
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM
    Terakhir setiap mahasiswa

---
+++ jobsheet7.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main (){
+	printf ("nama : FEBRIAN RIZKY\n");
+    printf ("nim : F1B019053\n ");
+    printf ("kelompok : 11\n");
+
+int nim [3], i;
+for (i = 0; i<= 3; i++){
+printf ("Masukkan nimke-%i ", i+1);
+scanf ("%i", &nim[i]);
+}
+}
