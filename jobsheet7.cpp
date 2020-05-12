eb39746bc5c52352077e6a74a723ce03f70f6e88
FEBRIAN RIZKY_F1B019053 <64627935+Febrianrizky24@users.noreply.github.com>
2020-05-12 18:50:18 +0800

    Create jobsheet7.cpp
    Buatlah program mengisi nilai array dengan perulangan dengan index array dan batas pengulangan menggunakan NIM
    Terakhir setiap mahasiswa

---
+++ jobsheet7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main (){
+	cout<<"nama:febrian rizky"<<endl;
+	cout<<"nim:F1B019053"<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+
+int nim [3];
+for (int i = 0; i<= 3; i++){
+cout<< "Masukkan nimke- " << i+1;
+cin>>nim[i];
+}
+}
