27a0ff9ef99f1fcded5b0555e4c75e191783894f
FEBRIAN RIZKY_F1B019053 <64627935+Febrianrizky24@users.noreply.github.com>
2020-05-12 17:59:37 +0800

    Create jobsheet1.cpp
    membuat program array satu dimensi yang menampilkan nim masing2 mahasiswa

---
+++ jobsheet1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+	
+int main(){
+	cout<<"nama:febrian rizky"<<endl;
+	cout<<"nim:F1B019053"<<endl;
+	cout<<"kelompok:11"<<endl;
+	
+	int NIM[13] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+	cout << "data ke-1 = " << NIM[0] << endl;
+	cout << "data ke-2 = " << NIM[0] << endl;
+	cout << "data ke-3 = " << NIM[0]<< endl;
+	cout << "data ke-4 = " << NIM[0]<< endl;
+	cout << "data ke-5 = " << NIM[0]<< endl;
+	cout << "data ke-6 = " << NIM[0]<< endl;
+	cout << "data ke-7 = " << NIM[0]<< endl;
+	cout << "data ke-8 = " << NIM[0]<< endl;
+	cout << "data ke-9 = " << NIM[0]<< endl;
+	cout << "data ke-10 = " << NIM[0]<< endl;
+	cout << "data ke-11 = " << NIM[0]<< endl;
+	cout << "data ke-12 = " << NIM[0]<< endl;
+	cout << "data ke-13 = " << NIM[0]<< endl;
+}
